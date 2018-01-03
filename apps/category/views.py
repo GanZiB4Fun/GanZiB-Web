cca0849fb5ae394596fa5e01204fe87f501539ad
GanZiB <ganzib4fun@163.com>
2018-01-03 17:34:53 +0800

    详情页增加 上一页下一页功能

--- apps/category/views.py
+++ apps/category/views.py
@@ -6,7 +6,7 @@
 # @Site    : 
 # @File    : views.py
 # @Software: PyCharm
-from flask import Blueprint, request, render_template, redirect, flash, session
+from flask import Blueprint, render_template
 from sqlalchemy import text
 
 from apps.book.models import Book
@@ -20,5 +20,6 @@ category = Blueprint('category', __name__, url_prefix='/category')
 def get_books_by_category_id(category_id=1):
     category_entry = sn.query(Category).filter(text('category_id=:category_id')).params(category_id=category_id).first()
     category_name = category_entry.category
-    books = sn.query(Book).filter(text('category=:category')).params(category=category_name).all()
+    books = sn.query(Book).filter(text('category=:category and status=:status')).params(category=category_name,
+                                                                                        status='YES').all()
     return render_template('books_list.html', title=category_name + '--小说列表', books=books)
--- apps/sections/views.py
+++ apps/sections/views.py
@@ -20,9 +20,22 @@ section = Blueprint('section', __name__, url_prefix='/section')
 @section.route('/<int:book_id>/<int:section_order>')
 def get_content(book_id, section_order):
     if book_id and section_order:
+        section_max = sn.query(Sections.section_order).filter(text('book_id=:book_id')).params(book_id=book_id).count()
         result_section = sn.query(Sections).filter(text('book_id=:book_id and section_order=:section_order')).params(
             book_id=book_id, section_order=section_order).first()
         path = result_section.path
+        if section_order > 1:
+            previous_order = section_order - 1
+            previous_section = "/section/" + str(book_id) + "/" + str(previous_order)
+        else:
+            previous_section = None
+
+        if (section_order + 1) <= section_max:
+            next_order = section_order + 1
+            next_section = "/section/" + str(book_id) + "/" + str(next_order)
+        else:
+            next_section = None
+
         if os.path.exists(path):
             title = result_section.title
             content = ''
@@ -30,7 +43,8 @@ def get_content(book_id, section_order):
                 for line in fp:
                     content = content + line.replace("\n", '<br/>')
                 fp.close()
-                return render_template('section_info.html', title=title, content=content)
+                return render_template('section_info.html', title=title, content=content, next_section=next_section,
+                                       previous_section=previous_section)
         else:
             return render_template('404.html')
     else:
